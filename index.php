dc8e25e7ec2fe5711493416a2fbf2b9ed7e5efbe
Harmanpreet-Singh <hermanpreetsandhu@gmail.com>
2017-08-22 22:29:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = 10;
+
+function myTest() {
+    global $x, $y;
+    $y = $x + $y;
+} 
+
+myTest();  // run function
+echo $y; // output the new value for variable $y
+?>
+
+</body>
+</html>
