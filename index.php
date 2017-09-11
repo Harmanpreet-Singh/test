61b216ed8a30e1324c33d761f0890fe50140352c
Harmanpreet-Singh <hermanpreetsandhu@gmail.com>
2017-09-11 12:58:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,32 @@
 
     	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ex at eros auctor pellentesque sed eu elit. In hac habitasse platea dictumst. Integer feugiat quam sed massa tempor sodales. Duis egestas non sem tincidunt mollis. Vivamus et egestas purus, ut sagittis nunc. Integer tincidunt nec nisl in tristique. Aliquam laoreet consectetur porttitor. Phasellus ut accumsan sem, pretium viverra lorem. Phasellus id pharetra leo. Sed tempus at augue ac ullamcorper.</p>
 
+	    
+	    
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+?>
+	    
+	    
+	    
+	    
     	<p>Sincerely,<br>
 		<strong><?php echo "$name"; ?></strong>
     	</p>
