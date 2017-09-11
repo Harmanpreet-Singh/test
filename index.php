91487f2539a7fe29d981e5ab73dcfea3fb3932ce
Harmanpreet-Singh <hermanpreetsandhu@gmail.com>
2017-09-11 14:22:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,20 @@ if (!pg_num_rows($result)) {
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
 print "\n";
-
+		
+		
+		
+$result = pg_query($pg_conn, "Select * from salesforce.Contact");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+		
+		
 ?>
 	    
 	    
